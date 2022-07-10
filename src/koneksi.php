81aeb6fc976eb76c641f13d7b8f42715b5f4e4f8
aguskertha <baguskertha@gmail.com>
2022-07-10 13:14:16 +0700

    new docker config

--- src/koneksi.php
+++ src/koneksi.php
@@ -2,5 +2,5 @@
     // session_start();    
     //membuat koneksi ke database
     // $conn = mysqli_connect("localhost","root","example","ta");
-    $conn = new mysqli("db", "root", "example", "ta");
+    $conn = new mysqli("db", "root", "root", "ta");
 ?>
