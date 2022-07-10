32b3175f850e595e27a52c5d914721de47e1c676
aguskertha <baguskertha@gmail.com>
2022-07-10 19:45:09 +0700

    last update

--- src/koneksi.php
+++ src/koneksi.php
@@ -2,5 +2,5 @@
     // session_start();    
     //membuat koneksi ke database
     // $conn = mysqli_connect("localhost","root","example","ta");
-    $conn = new mysqli("db", "root", "root", "ta");
+    $conn = new mysqli("localhost", "u4rfwhbx_root", "123@Webta", "u4rfwhbx_ta");
 ?>
